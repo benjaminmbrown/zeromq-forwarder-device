b54d67fb4b3140497012ddfce0df9b41998dd56e
Benjamin M. Brown <benjaminbb@gmail.com>
2016-02-09 19:38:03 -0600

    Add comments for forwarder

--- zmq-forwarder-device.py
+++ zmq-forwarder-device.py
@@ -1,18 +1,22 @@
 import zmq
 
+#PUBLISHERS -> fe SUB socket - FORWARDER - be PUB socket <-- SUBSCRIBERS
 def main():
 	context= zmq.Context()
 
 
  	try:
+ 		#create front end SUB socket for the forwarder coming from PUBLISHER
 		fe = context.socket(zmq.SUB)
 		fe.bind("tcp://*:5559")
 		fe.setsockopt(zmq.SUBSCRIBE, "")
 
+		#create backend PUB socket for the forwarder coming from SUBSCRIBER
 		be = context.socket(zmq.PUB)
 		backend.bind("tcp://*:5560")
 
 		zmq.device(zmq.FORWARDER, frontend, backend)
+		
 	except Exception, e:
 		print e
 		print "Destroying ZMQ forwarder forwarder device."
